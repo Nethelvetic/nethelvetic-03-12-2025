cdca684c7dc0c2e860f847320af3b17036f40db9
Golliard <golliard.info@gmail.com>
2025-03-18 14:29:52 +0100

    dixième commit

--- app/admin/evenements/[modif_id]/page.tsx
+++ app/admin/evenements/[modif_id]/page.tsx
@@ -6,7 +6,6 @@ import PageAdmEventModif from '../../../components/page-Adm-Event-Modif';
 
 
 export default function ModificationFormation() {
-  const { modif_id } = useParams();
 
   return (
     <div className="w-full md:w-5xl mx-auto">

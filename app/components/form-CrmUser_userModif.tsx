d039d4611582928caa4bfe702586a01423ec3d56
Nethelvetic Jérome Golliard <golliard73@gmail.com>
2025-05-26 21:36:40 +0200

    style: unify upload button container

--- app/components/form-CrmUser_userModif.tsx
+++ app/components/form-CrmUser_userModif.tsx
@@ -137,8 +137,9 @@ const FormCrmUser_userModif: React.FC = () => {
         console.error("2.0.5 ../../ FRONT FormCrmUserModif => useE => selectionOneCrmUser NO OK");
       }
     }
+    if (!modifIdStr) return;
     fetchUser();
-  }, []);
+  }, [modifIdStr, modifId]);
 
 
   //---------------------------------------------------------------------
@@ -271,13 +272,14 @@ const FormCrmUser_userModif: React.FC = () => {
                     ref={inputFileRef}
                     className="hidden"
                   />
-                  <button
-                    type="button"
-                    onClick={() => inputFileRef.current?.click()}
-                    className="mt-1 block w-full border border-gray-300 shadow-sm px-4 py-2"
-                  >
-                    Charger une image
-                  </button>
+                  <ContBtnLgNoEffectBgG>
+                    <button
+                      type="button"
+                      onClick={() => inputFileRef.current?.click()}
+                    >
+                      Charger une image
+                    </button>
+                  </ContBtnLgNoEffectBgG>
                 </div>
               </div>
               {/* Champs */}

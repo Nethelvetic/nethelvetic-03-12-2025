3bdada6e42db7c270e3b4517895591f72af0e680
Nethelvetic Jérome Golliard <golliard73@gmail.com>
2025-05-27 16:46:09 +0200

    fix(forms): adjust crm user form mini layout

--- app/components/form-CrmUser_user.tsx
+++ app/components/form-CrmUser_user.tsx
@@ -111,7 +111,7 @@ const FormCrmUser_user: React.FC = () => {
   const MiniUserForm = () => (
     <form className="space-y-4 p-4 w-full">
       <div className="flex flex-col md:flex-row items-start md:items-stretch p-3">
-        <div className="w-full md:w-1/3 mx-auto md:mx-0 flex flex-col h-80 md:h-full overflow-hidden">
+        <div className="w-full md:w-1/3 mx-auto md:mx-0 flex flex-col h-80 md:h-104 overflow-hidden">
           <div className="w-full overflow-hidden mt-15">
             <img
               src={unUserData.imgUrl}
@@ -130,6 +130,16 @@ const FormCrmUser_user: React.FC = () => {
                 setUnUserData({ ...unUserData, email: e.target.value })
               }
               className={`w-full border-b ${getInputClass(unUserData.email)}`}
+            />
+          </div>
+          <div>
+            <label>Nom de l'entreprise</label>
+            <input
+              value={unUserData.nom_entreprise}
+              onChange={e =>
+                setUnUserData({ ...unUserData, nom_entreprise: e.target.value })
+              }
+              className={`w-full border-b ${getInputClass(unUserData.nom_entreprise)}`}
             />
           </div>
           <div>
